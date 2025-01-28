70f00a2a9351f420b8fd2343225c4053e4b07fc9
youssefCOD <youssefamhendpro@gmail.com>
2025-01-28 19:57:59 +0100

    setting up the env

---
+++ src/main/java/com/example/CarsPriceHistory/CarsPriceHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarsPriceHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsPriceHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsPriceHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarsPriceHistory/CarsPriceHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarsPriceHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsPriceHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
